d664dd859d094d2a95b0efea93d6406755cb9cd5
Murutu <murutupeter@email.com>
2020-01-12 21:41:19 +0300

    Added manager.shell in manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,22 @@
+# Import db from app factory
 from app import create_app, db
 from flask_script import Manager,Server
 from app.models import *
 
 from flask_migrate import Migrate,MigrateCommand
 
+# Creating app instance
 app = create_app('test')
+app = create_app('development')
+app = create_app('production')
+
+# Create manager instance
+
+
+@manager.shell
+def make_shell_context():
+    return dict ( app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
+
+if __name__ == '__main__':
+    manager.run()
+
