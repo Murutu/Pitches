3008454657b9151d9f7980a028c7fbbfb371c2f4
Murutu <murutupeter@email.com>
2020-01-12 21:59:42 +0300

    Added import Flask in __init__.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # Import db from app factory
-from app import create_app, db
+from app import create_app,db
 from flask_script import Manager,Server
-from app.models import *
+from app.models import User
 
 from flask_migrate import Migrate,MigrateCommand
 
