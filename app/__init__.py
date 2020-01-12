282c09ea538a9113dd95da6ad9094f4840ebf51c
Murutu <murutupeter@email.com>
2020-01-12 22:07:11 +0300

    Added function create_app in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
+
+#Instances of flask extensions
+#Instance of loginManager and using its methods
+login_manager = LoginManager()
+
+
+
+def create_app(config_name):
+    '''
+    Function that takes configuration setting key as an argument
+    
+    Args:
+    config_name : name of the configuration to be used
+    '''
